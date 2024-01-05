938b98fa12504a20445598d9c35b897e07a06f8b
Иван Гнедько <ignedko34@gmail.com>
2024-01-05 14:54:55 +0300

    setup metadata

---
+++ src/config/config.ts
@@ -0,0 +1,6 @@
+export const config = {
+  name: "Next trello",
+  description: `Collaborate, manage projects, and reach new productivity peaks. From
+  high rises to the home office, the way your team works is unique -
+  accomplish it all with next trello`,
+};
